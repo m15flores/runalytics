31043f4e05bf5752890098c64db494bf0b6e40ad
m15flores <mfloresromo01@gmail.com>
2025-12-07 18:57:52 +0100

    EP 3 : Cycle 4 : Repositories Activity, Lap and Weekly Metrics

---
+++ metrics-engine/src/main/java/com/runalitycs/metrics_engine/repository/ActivityMetricsRepository.java
@@ -0,0 +1,14 @@
+package com.runalitycs.metrics_engine.repository;
+
+import com.runalitycs.metrics_engine.entity.ActivityMetrics;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface ActivityMetricsRepository extends JpaRepository<ActivityMetrics, UUID> {
+    Optional<ActivityMetrics> findByActivityId(UUID activityId);
+    boolean existsByActivityId(UUID activityId);
+}
