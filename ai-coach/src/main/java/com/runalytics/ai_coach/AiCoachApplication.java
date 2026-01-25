ecbc9bf25b66ea38f9767f6e519cb7304e091d54
m15flores <mfloresromo01@gmail.com>
2026-01-25 16:00:41 +0100

    Ai Coach : innit commit

---
+++ ai-coach/src/main/java/com/runalytics/ai_coach/AiCoachApplication.java
@@ -0,0 +1,13 @@
+package com.runalytics.ai_coach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiCoachApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiCoachApplication.class, args);
+	}
+
+}
---
+++ ai-coach/src/test/java/com/runalytics/ai_coach/AiCoachApplicationTests.java
@@ -0,0 +1,13 @@
+package com.runalytics.ai_coach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiCoachApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
