ba7e00123ce0de17fec72720b0486cf4e93b0f0c
m15flores <mfloresromo01@gmail.com>
2026-02-19 08:10:39 +0100

    activity-normalizer : fix delay - add DirtiesContext

--- activity-normalizer/src/test/java/com/runalitycs/normalizer/ActivityNormalizerApplicationTests.java
+++ activity-normalizer/src/test/java/com/runalitycs/normalizer/ActivityNormalizerApplicationTests.java
@@ -2,6 +2,7 @@ package com.runalitycs.normalizer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.kafka.KafkaContainer;
@@ -12,6 +13,7 @@ import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest
 @Testcontainers
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 class ActivityNormalizerApplicationTests {
 
 	@Container
