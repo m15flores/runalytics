8a6320394fe49d8503b47553712447f49b0db5cf
m15flores <mfloresromo01@gmail.com>
2026-03-06 13:54:12 +0100

    frontend: add env config for local and production

---
+++ frontend/src/environments/environment.production.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  activityServiceUrl: 'https://TODO_PRODUCTION_URL',
+  metricsServiceUrl:  'https://TODO_PRODUCTION_URL',
+  reportServiceUrl:   'https://TODO_PRODUCTION_URL',
+  coachServiceUrl:    'https://TODO_PRODUCTION_URL',
+};
---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  activityServiceUrl: 'http://localhost:8082',
+  metricsServiceUrl:  'http://localhost:8083',
+  reportServiceUrl:   'http://localhost:8084',
+  coachServiceUrl:    'http://localhost:8085',
+};
